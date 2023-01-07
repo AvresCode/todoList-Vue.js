a1bb3561ad1cf2fd99f2a9e5883812fd83dcf929
AvresCode <servas230@gmail.com>
2023-01-07 22:48:16 +0100

    Added logic to create task when submitting the form

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,16 +1,21 @@
 <template>
-  <form class="add-form">
+  <form @submit="onSubmit" class="add-form">
     <div class="form-control">
       <label> Task</label>
-      <input type="text" name="text" placeholder="Add Task" />
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
     </div>
     <div class="form-control">
       <label> Day & Time</label>
-      <input type="text" name="day" placeholder="Add day & time" />
+      <input
+        type="text"
+        v-model="day"
+        name="day"
+        placeholder="Add day & time"
+      />
     </div>
     <div class="form-control form-control-check">
       <label> Set Reminder</label>
-      <input type="checkbox" name="reminder" />
+      <input type="checkbox" v-model="reminder" name="reminder" />
     </div>
     <input type="submit" value="Save Task" class="btn btn-block" />
   </form>
@@ -19,6 +24,31 @@
 <script>
 export default {
   name: "AddTask",
+  data() {
+    return {
+      text: "",
+      day: "",
+      reminder: false,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (!this.text) {
+        alert("Please add a task");
+        return;
+      }
+
+      const newTask = {
+        id: Math.floor(Math.random() * 100),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
+      console.log("new task:", newTask);
+      (this.text = ""), (this.day = ""), (this.reminder = false);
+    },
+  },
 };
 </script>
 
